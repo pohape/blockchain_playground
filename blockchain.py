09102b46ad1a78a1911ce6a456c86c5c9cdebb5d
Pavel Gev <pavelgeveiler@gmail.com>
2023-09-01 15:16:50 +0200

    Quit

--- blockchain.py
+++ blockchain.py
@@ -10,19 +10,42 @@ def add_value(transaction_amount, last_transaction=[1]):
     print(blockchain)
 
 
-def get_user_input():
+def get_transaction_value():
     return float(input("Your transaction amount please:"))
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choise():
+    return input("Your choice: ")
 
-while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
 
+def print_blockchain_elements():
     for block in blockchain:
         print("Outputing Block")
         print(block)
+
+
+tx_amount = get_transaction_value()
+add_value(tx_amount)
+
+while True:
+    print("Please choose")
+    print("1: Add a new transaction")
+    print("2: Output the blockchain blocks")
+    print("q: Quit")
+
+    user_choice = get_user_choise()
+
+    if user_choice == "1":
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    elif user_choice == "2":
+        print_blockchain_elements()
+    elif user_choice == "q":
+        break
+    else:
+        print("Input was invalid")
+
+    print("Choice registered")
+
 
 print("Done!")
